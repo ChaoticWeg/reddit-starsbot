a5b5bf706004f06e403334ea73954784788177de
ChaoticWeg <shawn@chaoticweg.cc>
2019-02-28 11:06:23 -0600

    extract creds parsing out of logic

---
+++ tweets/tweetslib/creds.py
@@ -0,0 +1,22 @@
+import os
+
+__KEYS = [
+    "REDDIT_CLIENT_ID",
+    "REDDIT_CLIENT_SECRET",
+    "REDDIT_USERNAME",
+    "REDDIT_PASSWORD",
+    "REDDIT_USER_AGENT"
+]
+
+def get():
+    result = {}
+
+    for name in __KEYS:
+        value = os.getenv(name)
+        if value is None:
+            raise Exception(f"missing env for cred: {name}")
+        name_key = name.lower()[7:]
+        result[name_key] = value
+    
+    return result
+
--- tweets/tweetslib/scrape_reddit.py
+++ tweets/tweetslib/scrape_reddit.py
@@ -1,24 +1,10 @@
-import os
+from . import creds as __creds
+import praw
 
-__CREDS_KEYS = [ "REDDIT_CLIENT_ID", "REDDIT_CLIENT_SECRET", "REDDIT_USERNAME", "REDDIT_PASSWORD", "REDDIT_USER_AGENT" ]
-
-def __get_creds(user_agent=None):
-    result = {}
-
-    for name in __CREDS_KEYS:
-
-        # get value from environ
-        value = os.getenv(name)
-        if value is None:
-            raise Exception(f"missing value for credential env: {name}")
-
-        # convert to PRAW kwargs-friendly dict
-        name_key = name.lower()[7:]
-        result[name_key] = value
-    
-    return result
-    
 def get_tweet_posts():
-    creds = __get_creds()
-    print(creds)
+    creds = __creds.get()
+
+    reddit = praw.Reddit(**creds)
+    print(f"scraping posts as u/{reddit.user.me()}")
+
     return None
