c14cd382fb9c84c7a83c3ac683169a132f84d3af
ChaoticWeg <shawn@chaoticweg.cc>
2019-02-28 18:52:21 -0600

    add rudimentary json save/load

--- tweets/tweetslib/__init__.py
+++ tweets/tweetslib/__init__.py
@@ -1,1 +1,2 @@
 from .scrape_reddit import get_tweet_posts
+from .serialize_me import read, write
---
+++ tweets/tweetslib/serialize_me.py
@@ -0,0 +1,12 @@
+import json
+
+def read(filename, default=None):
+    try:
+        with open(filename, 'r') as infile:
+            return json.load(infile)
+    except FileNotFoundError:
+        return default
+
+def write(obj, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(obj, outfile)
